d02fe2bba10f9537ed680f5b09bb37de0beadcb6
SeonghunCho <acarlos93@snu.ac.kr>
2024-10-30 23:41:52 +0900

    Simulation 1 code update

--- code/Simulation1.R
+++ code/Simulation1.R
@@ -8,11 +8,13 @@ library(stats)
 nCore <- 1
 ########################################################
 wd <- "SET_WD"
-source(paste0(wd,"code/functions_sim.R"))
+source(paste0(wd,"code/functions_sim1.R"))
 ########################################################
-method_vec <- c("CCA","VAL","CEL1","CEL2","CEL3","MCEL")
-for(method in method_vec){
-  for(PS in 1:4){
+method_vec <- c("CCA","VAL","IMP","CEL1","CEL2","CEL3","MCEL")
+PS_vec <- 1:4
+
+for(i1 in 1:length(method_vec)){
+  for(i2 in 1:length(PS_vec)){
     ########################################################
     ## Basic Setting
     ########################################################
@@ -26,7 +28,8 @@ for(method in method_vec){
     s2x <- 1/3
     mu0 <- sum(beta0*c(1,mux1,mux2))
     alpha_quan <- 0.75
-    quan0 <- qnorm(p=alpha_quan,mean=mu0,sd=sqrt( sum(beta0[2:3]^2)*s2x+s20 ))
+    sd0 <- sqrt( sum(beta0[2:3]^2)*s2x+s20 )
+    quan0 <- qnorm(p=alpha_quan,mean=mu0,sd=sd0)
     theta0 <- c(beta0,mu0,quan0)
     ########################################################
     ## Bootstrap index
@@ -47,6 +50,8 @@ for(method in method_vec){
     ########################################################
     ## Main
     ########################################################
+    method <- method_vec[i1]
+    PS     <- PS_vec[i2]
     RES <- NULL
     for(sim_num in 1:1000){
       cat(method,"-",PS," : ",sim_num,"th iter\n",sep="")
@@ -57,14 +62,14 @@ for(method in method_vec){
       X1vec <- mux1+rnorm(n,0,sqrt(s2x))
       X2vec <- mux2+rnorm(n,0,sqrt(s2x))
       evec <- rnorm(n,0,sqrt(s20))
-      Xmat <- cbind(1,X1vec,X2vec)
-      yvec <- Xmat%*%beta0+evec
+      Xmat <- cbind(X1vec,X2vec)
+      yvec <- cbind(1,Xmat)%*%beta0+evec
       if(PS==1){
         phi <- c(0.879,0.5,-0.5)
         Zmat <- cbind(1,X1vec,X2vec)
         etavec <- c(Zmat%*%phi)
       }else if(PS==2){
-        phi <- c(0.857,0.5,-0.25)
+        phi <- c(-0.114,0.5,0.25)
         Zmat <- cbind(1,X1vec,yvec)
         etavec <- c(Zmat%*%phi)
       }else if(PS==3){
@@ -73,103 +78,114 @@ for(method in method_vec){
         etavec <- c(Zmat%*%phi)
       }else if(PS==4){
         cutvalue <- mu0
-        phi1 <- c(0.879,0.5,-0.5)
-        phi2 <- c(0.857,0.5,-0.25)
-        Zmat1 <- cbind(1,X1vec,X2vec)
-        Zmat2 <- cbind(1,X1vec,yvec)
+        phi1 <- c(0.857,0.5,-0.25)
+        phi2 <- c(0.865,0.5,-0.25)
+        Zmat1 <- cbind(1,X1vec,yvec)
+        Zmat2 <- cbind(1,X2vec,yvec)
         etavec1 <- c(Zmat1%*%phi1)
         etavec2 <- c(Zmat2%*%phi2)
         etavec <- rep(0,n)
-        etavec[yvec<cutvalue] <- etavec1[yvec<cutvalue]
+        etavec <- etavec1
         etavec[yvec>=cutvalue] <- etavec2[yvec>=cutvalue]
       }
       Prvec <- 1/(1+exp(-etavec))
       Uvec <- runif(n,0,1)
       deltavec <- (Uvec<=Prvec)
-      X1svec <- X1vec[deltavec]
-      X2svec <- X2vec[deltavec]
-      ysvec <- yvec[deltavec]
-      Prsvec <- Prvec[deltavec]
       n1 <- sum(deltavec)
       n0 <- n-n1
+      Bmat <- cbind(1,X1vec,X2vec)
       ########################################################
       ## Validation sample
       ########################################################
       X1val <- mux1+rnorm(nval,0,sqrt(s2x))
       X2val <- mux2+rnorm(nval,0,sqrt(s2x))
       eval <- rnorm(nval,0,sqrt(s20))
-      Xval <- cbind(1,X1val,X2val)
-      yval <- Xval%*%beta0+eval
+      Xval <- cbind(X1val,X2val)
+      yval <- cbind(1,Xval)%*%beta0+eval
       ########################################################
       ## Methods
       ########################################################
       if(method=="VAL"){
         ##########################################
         thetahat <- VAL(Xval=Xval,yval=yval,alpha=alpha_quan)
-        thetaBS <- mclapply(1:nBS,function(bnum){
+        thetaBS <- lapply(1:nBS,function(bnum){
           indb <- indBSval[,bnum]
-          return(VAL(Xval=Xval[indb,],yval=yval[indb],alpha=alpha_quan))
-        },mc.preschedule=F,mc.cores=nCore)
+          thetaBS_tmp <- VAL(Xval=Xval[indb,],yval=yval[indb],alpha=alpha_quan)
+          return(thetaBS_tmp)
+        })
         thetaBS <- do.call("rbind",thetaBS)
-        
       }else if(method=="CCA"){
         ##########################################
         thetahat <- CCA(Xmat=Xmat,yvec=yvec,deltavec=deltavec,alpha=alpha_quan)
-        thetaBS <- mclapply(1:nBS,function(bnum){
+        thetaBS <- lapply(1:nBS,function(bnum){
           indb <- indBS[,bnum]
-          return(CCA(Xmat=Xmat[indb,],yvec=yvec[indb],deltavec=deltavec[indb],alpha=alpha_quan))
-        },mc.preschedule=F,mc.cores=nCore)
+          thetaBS_tmp <- CCA(Xmat=Xmat[indb,],yvec=yvec[indb],deltavec=deltavec[indb],alpha=alpha_quan)
+          return(thetaBS_tmp)
+        })
+        thetaBS <- do.call("rbind",thetaBS)
+      }else if(method=="IMP"){
+        ##########################################
+        nuisval <- VALnuis(Xval=Xval,yval=yval)
+        thetahat <- IMP(nuis=nuisval,Xmat=Xmat,yvec=yvec,deltavec=deltavec,alpha=alpha_quan)
+        thetaBS <- lapply(1:nBS,function(bnum){
+          indb <- indBSval[,bnum]
+          nuisval <- VALnuis(Xval=Xval[indb,],yval=yval[indb])
+          indb <- indBS[,bnum]
+          thetaBS_tmp <- IMP(nuis=nuisval,Xmat=Xmat[indb,],yvec=yvec[indb],
+                             deltavec=deltavec[indb],alpha=alpha_quan)
+          return(thetaBS_tmp)
+        })
         thetaBS <- do.call("rbind",thetaBS)
       }else if(method=="CEL1"){
         ##########################################
         Zmat <- cbind(1,X1vec,X2vec)
         Zsmat <- Zmat[deltavec,]
-        Aux <- cbind(X1vec-mean(X1vec),X2vec-mean(X2vec))[deltavec,]
+        Aux <- scale(Xmat,center = T,scale = F)[deltavec,]
         thetahat <- CEL(Xmat=Xmat,yvec=yvec,deltavec=deltavec,Aux=Aux,
-                        is.mar=T,Zmat=Zmat,Zsmat=Zsmat,alpha=alpha_quan)
+                        is.mar=T,Zmat=Zmat,Zsmat=Zsmat,Bmat=Bmat,alpha=alpha_quan)
         thetaBS <- mclapply(1:nBS,function(bnum){
           indb <- indBS[,bnum]
           Zmat <- cbind(1,X1vec,X2vec)[indb,]
           Zsmat <- Zmat[deltavec[indb],]
-          Aux <- cbind(X1vec[indb]-mean(X1vec[indb]),
-                       X2vec[indb]-mean(X2vec[indb]))[deltavec[indb],]
-          return(CEL(Xmat=Xmat[indb,],yvec=yvec[indb],deltavec=deltavec[indb],Aux=Aux,
-                     is.mar=T,Zmat=Zmat,Zsmat=Zsmat,alpha=alpha_quan))
-        },mc.preschedule=F,mc.cores=nCore)
+          Aux <- scale(Xmat[indb,],center = T,scale = F)[deltavec[indb],]
+          thetaBS_tmp <- CEL(Xmat=Xmat[indb,],yvec=yvec[indb],deltavec=deltavec[indb],Aux=Aux,
+                             is.mar=T,Zmat=Zmat,Zsmat=Zsmat,Bmat=Bmat[indb,],alpha=alpha_quan)
+          return(thetaBS_tmp)
+        },mc.cores = nCore,mc.preschedule = FALSE)
         thetaBS <- do.call("rbind",thetaBS)
       }else if(method=="CEL2"){
         ##########################################
         Zmat <- cbind(1,X1vec)
         Zsmat <- cbind(Zmat,yvec)[deltavec,]
-        Aux <- cbind(X1vec-mean(X1vec),X2vec-mean(X2vec))[deltavec,]
+        Aux <- scale(Xmat,center = T,scale = F)[deltavec,]
         thetahat <- CEL(Xmat=Xmat,yvec=yvec,deltavec=deltavec,Aux=Aux,
-                        is.mar=F,Zmat=Zmat,Zsmat=Zsmat,alpha=alpha_quan)
+                        is.mar=F,Zmat=Zmat,Zsmat=Zsmat,Bmat=Bmat,alpha=alpha_quan)
         thetaBS <- mclapply(1:nBS,function(bnum){
           indb <- indBS[,bnum]
           Zmat <- cbind(1,X1vec)[indb,]
           Zsmat <- cbind(Zmat,yvec[indb])[deltavec[indb],]
-          Aux <- cbind(X1vec[indb]-mean(X1vec[indb]),
-                       X2vec[indb]-mean(X2vec[indb]))[deltavec[indb],]
-          return(CEL(Xmat=Xmat[indb,],yvec=yvec[indb],deltavec=deltavec[indb],Aux=Aux,
-                     is.mar=F,Zmat=Zmat,Zsmat=Zsmat,alpha=alpha_quan))
-        },mc.preschedule=F,mc.cores=nCore)
+          Aux <- scale(Xmat[indb,],center = T,scale = F)[deltavec[indb],]
+          thetaBS_tmp <- CEL(Xmat=Xmat[indb,],yvec=yvec[indb],deltavec=deltavec[indb],Aux=Aux,
+                             is.mar=F,Zmat=Zmat,Zsmat=Zsmat,Bmat=Bmat[indb,],alpha=alpha_quan)
+          return(thetaBS_tmp)
+        },mc.cores = nCore,mc.preschedule = FALSE)
         thetaBS <- do.call("rbind",thetaBS)
       }else if(method=="CEL3"){
         ##########################################
         Zmat <- cbind(1,X2vec)
         Zsmat <- cbind(Zmat,yvec)[deltavec,]
-        Aux <- cbind(X1vec-mean(X1vec),X2vec-mean(X2vec))[deltavec,]
+        Aux <- scale(Xmat,center = T,scale = F)[deltavec,]
         thetahat <- CEL(Xmat=Xmat,yvec=yvec,deltavec=deltavec,Aux=Aux,
-                        is.mar=F,Zmat=Zmat,Zsmat=Zsmat,alpha=alpha_quan)
+                        is.mar=F,Zmat=Zmat,Zsmat=Zsmat,Bmat=Bmat,alpha=alpha_quan)
         thetaBS <- mclapply(1:nBS,function(bnum){
           indb <- indBS[,bnum]
           Zmat <- cbind(1,X2vec)[indb,]
           Zsmat <- cbind(Zmat,yvec[indb])[deltavec[indb],]
-          Aux <- cbind(X1vec[indb]-mean(X1vec[indb]),
-                       X2vec[indb]-mean(X2vec[indb]))[deltavec[indb],]
-          return(CEL(Xmat=Xmat[indb,],yvec=yvec[indb],deltavec=deltavec[indb],Aux=Aux,
-                     is.mar=F,Zmat=Zmat,Zsmat=Zsmat,alpha=alpha_quan))
-        },mc.preschedule=F,mc.cores=nCore)
+          Aux <- scale(Xmat[indb,],center = T,scale = F)[deltavec[indb],]
+          thetaBS_tmp <- CEL(Xmat=Xmat[indb,],yvec=yvec[indb],deltavec=deltavec[indb],Aux=Aux,
+                             is.mar=F,Zmat=Zmat,Zsmat=Zsmat,Bmat=Bmat[indb,],alpha=alpha_quan)
+          return(thetaBS_tmp)
+        },mc.cores = nCore,mc.preschedule = FALSE)
         thetaBS <- do.call("rbind",thetaBS)
       }else if(method=="MCEL"){
         ##########################################
@@ -182,17 +198,19 @@ for(method in method_vec){
         is.mar <- rep(F,K)
         is.mar[1] <- T
         Zsmat.list <- list()
+        phihat.list <- list()
         for(k in 1:K){
           if(is.mar[k]){
             Zsmat.list[[k]] <- Zmat.list[[k]][deltavec,]
+            phihat.list[[k]] <- Estphi_MAR(Zmat.list[[k]],deltavec)
           }else{
             Zsmat.list[[k]] <- cbind(Zmat.list[[k]],yvec)[deltavec,]
+            phihat.list[[k]] <- Estphi(Bmat,Zsmat.list[[k]],deltavec)
           }
         }
-        Aux <- cbind(X1vec-mean(X1vec),X2vec-mean(X2vec))[deltavec,]
-        thetahat <- MCEL(nuis=nuisval,
-                         Xmat=Xmat,yvec=yvec,deltavec=deltavec,Aux=Aux,
-                         is.mar=is.mar,Zmat.list=Zmat.list,Zsmat.list=Zsmat.list,alpha=alpha_quan)
+        Aux <- scale(Xmat,center = T,scale = F)[deltavec,]
+        thetahat <- MCEL(nuis=nuisval,Xmat=Xmat,yvec=yvec,deltavec=deltavec,Aux=Aux,
+                         Zsmat.list=Zsmat.list,phihat.list=phihat.list,alpha=alpha_quan)
         thetaBS <- mclapply(1:nBS,function(bnum){
           indb <- indBSval[,bnum]
           nuisval <- VALnuis(Xval=Xval[indb,],yval=yval[indb])
@@ -203,33 +221,35 @@ for(method in method_vec){
           Zmat.list[[3]] <- cbind(1,X2vec)[indb,]
           K <- length(Zmat.list)
           Zsmat.list <- list()
+          phihat.list <- list()
           for(k in 1:K){
             if(is.mar[k]){
               Zsmat.list[[k]] <- Zmat.list[[k]][deltavec[indb],]
+              phihat.list[[k]] <- Estphi_MAR(Zmat.list[[k]],deltavec[indb])
             }else{
               Zsmat.list[[k]] <- cbind(Zmat.list[[k]],yvec[indb])[deltavec[indb],]
+              phihat.list[[k]] <- Estphi(Bmat[indb,],Zsmat.list[[k]],deltavec[indb])
             }
           }
-          Aux <- cbind(X1vec[indb]-mean(X1vec[indb]),
-                       X2vec[indb]-mean(X2vec[indb]))[deltavec[indb],]
-          return(MCEL(nuis=nuisval,
-                      Xmat=Xmat[indb,],yvec=yvec[indb],deltavec=deltavec[indb],Aux=Aux,
-                      is.mar=is.mar,Zmat.list=Zmat.list,Zsmat.list=Zsmat.list,alpha=alpha_quan))
-        },mc.preschedule=F,mc.cores=nCore)
+          Aux <- scale(Xmat[indb,],center = T,scale = F)[deltavec[indb],]
+          thetaBS_tmp <- MCEL(nuis=nuisval,Xmat=Xmat[indb,],yvec=yvec[indb],deltavec=deltavec[indb],Aux=Aux,
+                              is.mar=is.mar,Zmat.list=Zmat.list,Zsmat.list=Zsmat.list,
+                              phihat.list=phihat.list,alpha=alpha_quan)
+          return(thetaBS_tmp)
+        },mc.cores = nCore,mc.preschedule = FALSE)
         thetaBS <- do.call("rbind",thetaBS)
       }
       varBS <- diag(var(thetaBS))
       lb <- thetahat - qnorm(0.975)*sqrt(varBS)
       ub <- thetahat + qnorm(0.975)*sqrt(varBS)
       inc <- (lb <= theta0)&(theta0 <= ub)
-      res_total <- data.frame(n=n,PS=PS,
-                              sim_num=sim_num,
-                              method=method,
+      res_total <- data.frame(n=n,PS=PS,method=method,
+                              sim_num=sim_num,ratio=mean(deltavec),
                               parname=c(paste0("b",0:2),"mu","quan"),
                               theta=theta0,thetahat=thetahat,
                               varBS=varBS,inc=inc)
       RES <- rbind(RES,res_total)
     }
-    save(list=c("RES"),file=paste0(wd,"output/res_sim1/res1_",method,PS,".RData"))
+    save(list=c("RES"),file=paste0(wd,"output/res_sim1/res_",i1,i2,".RData"))
   }
 }
--- code/Simulation1_summary.R
+++ code/Simulation1_summary.R
@@ -5,29 +5,26 @@ library(reshape)
 library(gridExtra)
 wd <- "SET_WD"
 ##############################################################################
-PS_vec <- 1:4
-method_vec <- c("VAL","CCA","CEL1","CEL2","CEL3","MCEL")
+method_vec <- c("CCA","VAL","IMP","CEL1","CEL2","CEL3","MCEL")
 nmethod <- length(method_vec)
+PS_vec <- 1:4
 pn_vec <- c("mu","quan")
-
+##############################################################################
 RES_total <- NULL
-for(PS in PS_vec){
-  for(method in method_vec){
-    load(paste0(wd,"res_sim1/res1_",method,PS,".RData"))
-    RES$theta <- as.numeric(RES$theta)
-    RES$thetahat <- as.numeric(RES$thetahat)
+for(i1 in 1:length(method_vec)){
+  for(i2 in 1:length(PS_vec)){
+    load(file=paste0(wd,"output/res_sim1/res_",i1,i2,".RData"))
     RES_total <- rbind(RES_total,RES)
   }
 }
 ##############################################################################
 ## Table 1
 tt <- NULL
-for(PSt in PS_vec){
-  tt_PS <- cbind(c(paste0("\\multirow{",nmethod,"}{*}{RM",PSt,"}"),rep("",nmethod-1)),
-                 method_vec)
+for(i2 in 1:length(PS_vec)){
+  tt_PS <- cbind(c(paste0("\\multirow{",nmethod,"}{*}{RM",i2,"}"),rep("",nmethod-1)),method_vec)
   for(pn in pn_vec){
     RES_tmp <- RES_total %>%
-      filter(PS==PSt,parname==pn) %>%
+      filter(PS==i2,parname==pn) %>%
       mutate(method=factor(method,levels=method_vec)) %>%
       mutate(bias=thetahat-theta) %>%
       group_by(method) %>%
@@ -45,7 +42,7 @@ for(PSt in PS_vec){
 }
 tt <- apply(tt,1,function(v){paste(v,collapse=" & ")})
 tt <- paste(tt,collapse=" \\\\\n")
-cat(tt,file=paste0(wd,"summary/sim1_table.txt"))
+cat(tt,file=paste0(wd,"output/summary/sim1_table.txt"))
 ##############################################################################
 ## Figure 1
 get_legend <- function(myggplot){
@@ -54,14 +51,11 @@ get_legend <- function(myggplot){
   legend <- tmp$grobs[[leg]]
   return(legend)
 }
-
-PS_vec <- 1:4
-title_vec <- paste0("(",c("a","b","c","d"),") RM",1:4)
+title_vec <- paste0("(",c("a","b","c","d"),") RM",PS_vec)
 bp_total <- list()
-for(i in 1:length(PS_vec)){
-  PSt <- PS_vec[i]
+for(i2 in 1:length(PS_vec)){
   RES_tmp <- RES_total %>%
-    filter(PS==PSt,parname%in%pn_vec) %>%
+    filter(PS==PS_vec[i2],parname%in%pn_vec) %>%
     mutate(method = factor(method,levels=method_vec)) %>%
     mutate(bias = thetahat-theta) %>%
     mutate(PS=paste0("PS",PS)) %>%
@@ -71,7 +65,7 @@ for(i in 1:length(PS_vec)){
     geom_boxplot(aes(x=parname,y=bias,fill=method)) +
     geom_abline(slope = 0,intercept = 0,linetype="dashed") +
     theme_bw() + 
-    labs(caption=title_vec[i]) + 
+    labs(caption=title_vec[i2]) + 
     theme(plot.caption = element_text(hjust=0.5,size=15)) +
     theme(legend.position="bottom",
           legend.direction="horizontal") +
@@ -83,8 +77,8 @@ for(i in 1:length(PS_vec)){
 }
 bp_total <- append(bp_total,list(bp_legend))
 lay_mat <- rbind(matrix(1:4,2,2,byrow = T),5)
-ggsave(filename=paste0(wd,"summary/sim1_boxplot.pdf"),
+ggsave(filename=paste0(wd,"output/summary/sim1_boxplot.pdf"),
        plot=arrangeGrob(grobs=bp_total,nrow=3,ncol=2,layout_matrix=lay_mat,
                         widths=c(5,5),heights=c(3,3,1)),
-       width=10,height=6,device="pdf")
+       width=10,height=7,device="pdf")
 ##############################################################################
